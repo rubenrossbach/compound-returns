18990a16c0f7bae3474697702d367162b4b2564c
Ruben Rossbach <ruben.rossbach@gmx.net>
2022-04-22 12:31:47 +0200

    added plot

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 st.set_page_config(
     page_title="Compound Returns Calculator",
     page_icon="chart_with_upwards_trend",
-    layout="wide")
+    layout="centered")
 
-CSS = """
-iframe {
-    width: 100%
-}
-img {
-    display: block;
-    margin-left: center;
-    margin-right: center;
-    width: 50%;
-    border-radius: 50%;
-}
+# CSS = """
+# iframe {
+#     width: 50%
+# }
+# img {
+#     display: block;
+#     margin-left: center;
+#     margin-right: center;
+#     width: 50%;
+# }
 
-"""
+# """
 
-st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+# st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
 st.write("# Compound Return Calculator")
 
@@ -30,7 +32,7 @@ initial = st.number_input(
     step=1000.0
     )
 
-savings = st.number_input(
+monthly_savings = st.number_input(
     label='Monthly Savings',
     value=0.0,
     step=100.0
@@ -56,25 +58,67 @@ tax_rate = st.number_input(
 
 years = st.number_input(
     label='Number of Years',
-    value=0.0,
-    step=1.0
+    value=0,
+    step=1
     )
 
-# calculation
+# calculation and display
 if st.button('Calculate'):
-    I = initial
-    S = savings * 12
-    RS = 1 + savings_growth / 100
+
+    ## method 1
+    # I = initial
+    # S = savings * 12
+    # RS = 1 + savings_growth / 100
+    # net_returns = returns * (1 - tax_rate / 100)
+    # RI = 1 + net_returns / 100
+    # N = years - 1
+
+    # try:
+    #     result = I * RI**N \
+    #             + S * RI**N * ((RS/RI)**(N+1)-1) / ((RS/RI)-1)
+    #     st.metric('Result', f'{round(result,2):,}')
+    # except ZeroDivisionError:
+    #     st.write("""Zero Division Error: Choose different values
+    #              for net return and savings increase.""")
+
+    ## method 2
     net_returns = returns * (1 - tax_rate / 100)
-    RI = 1 + net_returns / 100
-    N = years - 1
 
-    try:
-        result = I * RI**N \
-                + S * RI**N * ((RS/RI)**(N+1)-1) / ((RS/RI)-1)
-        st.metric('Result', f'{round(result,2):,}')
-    except ZeroDivisionError:
-        st.write("""Zero Division Error: Choose different values
-                 for net return and savings increase.""")
+    # dataframe initialize
+    df = pd.DataFrame(
+        {
+            "Year": list(range(1, years + 1)),
+            "Previous": [initial] + [np.nan]*(years-1),
+            "Returns": [np.nan]*years,
+            "Invested": [np.nan]*years,
+            "End": [np.nan]*years
+        }
+    )
 
-# display: create bar chart
+    df = df.set_index("Year")
+
+    # iterate
+    for i in range(1, years + 1):
+        if i > 1:
+            df.loc[i, "Previous"] = end
+        df.loc[i, "Returns"] = df.loc[i, "Previous"] * net_returns / 100
+        df.loc[i, "Invested"] = (monthly_savings * 12) * (1 + savings_growth / 100)**i
+        end = df.loc[i].sum()
+        df.loc[i, "End"] = end
+
+    st.metric('Final amount', f'{round(df.iloc[-1,-1],2):,}')
+
+    # plot
+    width = 0.35
+    fig, ax = plt.subplots()
+
+    ax.bar(df.index, df.Previous, width, label='Previous')
+    ax.bar(df.index, df.Invested, width, bottom=df.Previous, label='Invested')
+    ax.bar(df.index, df.Returns, width, bottom=df.Previous + df.Invested, label='Gains')
+
+    ax.set_ylabel('$')
+    ax.set_xlabel('Year')
+    ax.set_title('Portfolio Development by Year')
+    ax.legend()
+    ax.set_xticks(df.index)
+    st.pyplot(fig)
